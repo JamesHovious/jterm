b1363809b1bb7a9c168e1a8080cde2ab7f6834e8
JamesHovious <hoviousj@gmail.com>
2017-08-30 20:34:44 +0200

    cleaning up. making the default values work

--- examples/main.go
+++ examples/main.go
@@ -3,8 +3,8 @@ package main
 import (
 	"fmt"
 	"time"
-	"github.com/JamesHovious/jterm"
 
+	"github.com/JamesHovious/jterm"
 )
 
 func main() {
@@ -12,7 +12,7 @@ func main() {
 	customTerminal()
 }
 
-func postCallbackFunction(command, term string) func() string{
+func postCallbackFunction(command, term string) func() string {
 	print("[-] Hello, I print out to the console. ")
 	print("[-] I get called everytime a new command is entered. ")
 	print("[-] The command was: " + command)
@@ -24,13 +24,11 @@ func postCallbackFunction(command, term string) func() string{
 // An minimal example
 func defaultTerminal() {
 	// Create an terminal with default properties.
-	jt := jterm.DefaultTerminal
+	jt := jterm.DefaultTerminal()
 	// Define the callback function
 	post := postCallbackFunction
-	// Locate the div with the id of "termina"
-	cID := "#terminal"
-	// create a Terminal/jQuery struct
-	JQueryTerminal := jterm.NewTerminal(cID)
+	// Locate the div with the id of "terminal"
+	JQueryTerminal := jterm.NewTerminal("#terminal")
 	// Finally put it all together. Create the terminal.
 	jterm.CallTerminal(JQueryTerminal, jt, post)
 }
@@ -43,18 +41,26 @@ func customTerminal() {
 	p := fmt.Sprintf("[[b;#FFFF00;#000][%v\\]:] ", t.Format("2006-01-02 15:04"))
 
 	// Create an terminal with default properties.
-	jt := jterm.DefaultTerminal
+	jt := jterm.DefaultTerminal()
 	// Populate the terminal with custom properties.
 	jt.Prompt = p
-	jt.Greetings = "Custom terminal banner!"
-	
+	jt.Name = "MyCustomTerminal"
+	jt.Greetings = "Custom Gopherjs Terminal banner!"
+
 	// Define the callback function
 	post := postCallbackFunction
-	// Locate the div with the id of "termina"
-	cID := "#custom-terminal"
-	// create a Terminal/jQuery struct
-	JQueryTerminal := jterm.NewTerminal(cID)
+
+	// Locate the div with the id of "terminal"
+	JQueryTerminal := jterm.NewTerminal("#custom-terminal")
+
 	// Finally put it all together. Create the terminal.
 	jterm.CallTerminal(JQueryTerminal, jt, post)
+
+	// Call methods on the same terminal, as needed.
+	JQueryTerminal.Echo("Example echo output")
+
+	// Note, right now methods that return values don't actually return a value...
+	tPrompt := JQueryTerminal.GetName()
+	JQueryTerminal.Echo("The current prompt is : " + tPrompt)
 
 }
--- jterm.go
+++ jterm.go
@@ -1,37 +1,20 @@
+// Package jterm is a binding of gopherjs to jquery.terminal. This lets you use jquery.terminal without writing
+// any JavaScript. There is the examples directory to show some simple use cases.
+// In some cases jqeury.terminal has properties and methods that have the same name. Go doesn't
+// allow this to happen. In these cases the method name may have been changed slightly in this package.
 package jterm
 
 import (
-	"reflect"
+	"strconv"
 
-	"unicode"
-	"unicode/utf8"
-
+	"github.com/gopherjs/gopherjs/js"
 	"github.com/gopherjs/jquery"
 )
 
-// thanks https://gist.github.com/derlin/0be53d0d7f38db181198aada024269b8
-func structToLowerFirstMap(in Terminal) map[string]interface{} {
-	v := reflect.ValueOf(in)
-	vType := v.Type()
-
-	result := make(map[string]interface{}, v.NumField())
-
-	for i := 0; i < v.NumField(); i++ {
-		name := vType.Field(i).Name
-		if name != "jq" {
-			result[lowerFirst(name)] = v.Field(i).Interface()
-		}
-	}
-	return result
-}
-
-// thanks https://gist.github.com/derlin/0be53d0d7f38db181198aada024269b8
-func lowerFirst(s string) string {
-	if s == "" {
-		return ""
-	}
-	r, n := utf8.DecodeRuneInString(s)
-	return string(unicode.ToLower(r)) + s[n:]
+// CallTerminal will call the jQuery.terminal method, which is the entry point for the library.
+func CallTerminal(j, t Terminal, post func(c, t string) func() string) Terminal {
+	j.jq.Call("terminal", post, t)
+	return t
 }
 
 // NewTerminal will return a new Terminal struct. It is a "superclass" of jquery.JQuery
@@ -39,32 +22,25 @@ func NewTerminal(args string) Terminal {
 	return Terminal{jq: jquery.NewJQuery(args)}
 }
 
-// CallTerminal will call the jQuery.terminal method, which is the entry point for the library.
-func CallTerminal(j, t Terminal, post func(c, t string) func() string) Terminal {
-	m := structToLowerFirstMap(t)
-	j.jq.Call("terminal", post, m)
-	return t
+// DefaultTerminal returns a Terminal struct with all of the default values pre-populated.
+func DefaultTerminal() Terminal {
+	obj := js.Global.Get("$").Get("terminal").Get("defaults")
+	t := &Terminal{o: obj}
+	return *t
 }
 
 // Terminal is a struct with fields that correspond to jquery.terminal options.
 type Terminal struct {
-	jq        jquery.JQuery
-	Greetings string
-	Prompt    interface{}
-	Name      string
-	ScrollBottomOffset int
-	ScrollOnEcho       bool
-	EchoCommand        bool
-}
-
-// DefaultTerminal contains all of the default values for the Terminal
-var DefaultTerminal = Terminal {
-	Greetings: "Interpreter with jterm and jquery.terminal",
-	Prompt: "> ",
-	Name: "jterm",
-	ScrollBottomOffset: 0,
-	ScrollOnEcho: true,
-	EchoCommand: true,
+	o                  *js.Object
+	jq                 jquery.JQuery
+	Greetings          string      `js:"greetings"`
+	History            bool        `js:"history"`
+	Exit               bool        `js:"exit"`
+	Prompt             interface{} `js:"prompt"`
+	Name               string      `js:"name"`
+	ScrollBottomOffset int         `js:"scollBottomOffse"`
+	ScrollOnEcho       bool        `js:"scrollOnEcho"`
+	EchoCommand        bool        `js:"echo"`
 }
 
 // Clear will clear the terminal.
@@ -94,9 +70,34 @@ func (t Terminal) Enable() {
 	t.jq.Call("enable")
 }
 
+// Error displays the input in red.
+func (t Terminal) Error(e interface{}) {
+	t.jq.Call("error", e)
+}
+
 // Disable disables the terminal.
 func (t Terminal) Disable() {
 	t.jq.Call("disable")
+}
+
+// Flush will echo all stored content to the terminal at once, if the `flush`` property is set to `false`.
+func (t Terminal) flush() {
+	t.jq.Call("flush")
+}
+
+// GetCommand will return the text for the current command.
+func (t Terminal) GetCommand() string {
+	return t.jq.Call("get_command").Val()
+}
+
+// GetPrompt willl return the current prompt.
+func (t Terminal) GetPrompt() string {
+	return t.jq.Call("get_prompt").Val()
+}
+
+// Insert will insert text intot he current cursor position.
+func (t Terminal) Insert(s string) {
+	t.jq.Call("insert", s)
 }
 
 // IsBottom returns true if the terminal is scrolled to the bottom.
@@ -104,10 +105,51 @@ func (t Terminal) IsBottom() bool {
 	return t.jq.Call("is_bottom").Is()
 }
 
+// Level will return how deeply nested in the interpreters you currently are.
+func (t Terminal) Level() int {
+	i, _ := strconv.Atoi(t.jq.Call("level").Val())
+	return i
+}
+
+// LoginName will reutrn the login name which was used in authetntication.
+func (t Terminal) LoginName() string {
+	return t.jq.Call("login_name").Val()
+}
+
+// Logout will log you out from the current terminal.
+func (t Terminal) Logout() {
+	t.jq.Call("logout")
+}
+
+// GetName will return the name of the current terminal. Renamed from Name() to GetName().
+func (t Terminal) GetName() string {
+	return t.jq.Call("name").Val()
+}
+
+// Paused will return true if the terminal is paused.
+func (t Terminal) Paused() bool {
+	return t.jq.Call("paused").Is()
+}
+
+// Pop will remove the current interpreter from the stack and run ht eprevious one.
+func (t Terminal) Pop() {
+	t.jq.Call("pop")
+}
+
+// Purge will remove all local storage left by the terminal.
+func (t Terminal) Purge() {
+	t.jq.Call("purge")
+}
+
 // Reset will reset the terminal
 func (t Terminal) Reset() {
 	// TODO this func does not fully impliment the jquery.terminal echo function
 	t.jq.Call("reset")
+}
+
+// SetCommand will set the current command.
+func (t Terminal) SetCommand(s string) {
+	t.jq.Call("set_command", s)
 }
 
 // ScrollToBottom will scroll to the bottom of the terminal.
